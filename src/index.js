e8f72084d611ca41f090aff4b27a41001cd39a33
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-05-08 12:43:58 -0400

    initial commit: sucessful render

--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from 'phaser';
+import square from './assets/square.png';
+import star from './assets/star.png';
+import diamond from './assets/diamond.png';
 
+// game config
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
   width: 800,
   height: 600,
   scene: {
-    preload: preload,
-    create: create
+      preload: preload,
+      create: create,
+      update: update
   }
 };
 
-const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image("logo", logoImg);
+let game = new Phaser.Game(config);
+
+// assets
+const assets = [
+  {label: 'star', filename: star},
+  {label: 'diamond', filename: diamond}
+];
+
+// load all assets
+// ===============
+function preload(){
+  // load images
+  for(const img of assets)
+  {
+      // load images by label and default or specified filename 
+      this.load.image(img.label, img.filename === undefined ? square : img.filename);
+  }
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+// initial render
+// ==============
+function create(){
+  // add image: x,y coords, label
+  this.add.image(400, 300, 'star');
+  this.add.image(400, 400, 'diamond');
 }
+
+function update(){}
