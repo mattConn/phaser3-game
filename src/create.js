6be6878aae8a866be060b261ff37e2d05f5175bf
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-05-08 21:49:25 -0400

    added static and dynamic groups for assigning physics props

--- src/create.js
+++ src/create.js
@@ -1,19 +1,39 @@
 import { config } from './index';
 
-let platformObj, player;
+// static (immovable) groups 
+const staticGroups = {
+  platforms: null 
+};
+
+// dynamic (moving) groups
+const dynamicGroups = {
+  player: null 
+};
+
 // initial render
 // ==============
 export function create() {
-  platformObj = this.physics.add.staticGroup();
-  platformObj.create(100, 100, 'platform');
-  platformObj.create(0, config.height, 'platform');
+  // scene setup
+  // ===========
 
-  player = this.physics.add.sprite(100, 450, 'dude');
+  // add static groups
+  for(const group in staticGroups)
+    staticGroups[group] = this.physics.add.staticGroup();
 
-  this.physics.add.collider(player, platformObj);
+  // player setup
+  dynamicGroups.player = this.physics.add.sprite(100, 450, 'dude');
+  dynamicGroups.player.setBounce(0.2);
+  dynamicGroups.player.setCollideWorldBounds(true);
 
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
+  // add collision
+  this.physics.add.collider(dynamicGroups.player, staticGroups.platforms);
+
+  // initial scene creation
+  // ======================
+
+  // create initial platforms
+  staticGroups.platforms.create(100, 100, 'platform');
+  staticGroups.platforms.create(0, config.height, 'platform');
 
   // add image: x,y coords, label
   this.add.image(0, 0, 'star').setOrigin(0);
