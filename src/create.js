4f96acac8f3400e35364a1e2235c8d7672079b9a
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-05-11 14:40:18 -0400

    added sprite adding function to dyn. objects, static objects create with offset

--- src/create.js
+++ src/create.js
@@ -3,21 +3,21 @@ import StaticObj from './gameObjects/StaticObj';
 import DynamicObj from './gameObjects/DynamicObj';
 
 
-let player;
+let player, platforms;
 export { player };
 
 
 // scene setup 
 export default function create() {
   // platforms setup
-  let platforms = new StaticObj(this, 'platform');
-  platforms.width = 400;
-  platforms.height = 32;
+  platforms = new StaticObj(this, 'platform', 400, 32);
 
-  platforms.create(platforms.width/2, config.height - platforms.height/2);
+  platforms.create(0, config.height - platforms.height);
+  platforms.create(platforms.width, config.height - platforms.height);
 
   // player setup
-  player = new DynamicObj(this, 'dude', 100, 300, platforms);
+  player = new DynamicObj(this, 'dude', 32, 48, platforms);
+  player.addSprite(100,400);
   player.addAnimation(this,'walk', 0, 3, 10);
   player.addAnimation(this,'idle', 0, 0);
   player.addAnimation(this,'inTheAir', 1, 1);
--- src/update.js
+++ src/update.js
@@ -3,10 +3,10 @@ import { config } from './index';
 
 export default function update(){
 
-    if(player.group.x > config.width)
-      player.group.x = 0;
-    if(player.group.x < 0)
-      player.group.x = config.width;
+    if(player.sprite.x > config.width)
+      player.sprite.x = 0;
+    if(player.sprite.x < 0)
+      player.sprite.x = config.width;
 
     let cursors = this.input.keyboard.createCursorKeys();
 
@@ -15,20 +15,20 @@ export default function update(){
 
       // FLIPPING SPRITE HERE
     if (cursors.left.isDown) {
-      player.group.setVelocityX(-160);
-      player.group.flipX = false;
+      player.sprite.setVelocityX(-160);
+      player.sprite.flipX = false;
     } else if (cursors.right.isDown) {
-      player.group.setVelocityX(160);
-      player.group.flipX = true;
+      player.sprite.setVelocityX(160);
+      player.sprite.flipX = true;
     } else {
-      player.group.setVelocityX(0);
+      player.sprite.setVelocityX(0);
       player.playAnimation('idle');
     }
   
-    if (cursors.up.isDown && player.group.body.touching.down) {
-      player.group.setVelocityY(-250);
+    if (cursors.up.isDown && player.sprite.body.touching.down) {
+      player.sprite.setVelocityY(-250);
     }
 
-    if (!player.group.body.touching.down) 
+    if (!player.sprite.body.touching.down) 
       player.playAnimation('inTheAir');
 }
