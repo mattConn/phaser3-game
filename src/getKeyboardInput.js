10b8150069e051ef63fd275760c807805f2b0e46
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-05-14 15:20:00 -0400

    additional comments for getKeyboardInput

--- src/getKeyboardInput.js
+++ src/getKeyboardInput.js
@@ -1,27 +1,31 @@
 import { player } from './create';
-import { config } from './index';
 
+// keyboard control: change player's velcocity, animations
 export default function getKeyboardInput(game){
     let cursors = game.input.keyboard.createCursorKeys();
 
+    // left or right key: play walk animation
     if (cursors.left.isDown || cursors.right.isDown)
       player.playAnimation('walk');
 
-    if (cursors.left.isDown) {
+    // set positive or negative velocity on player, flip horizontally
+    if (cursors.left.isDown) { // move left
       player.sprite.setVelocityX(-280);
       player.sprite.flipX = false;
-    } else if (cursors.right.isDown) {
+    } else if (cursors.right.isDown) { // move right
       player.sprite.setVelocityX(280);
       player.sprite.flipX = true;
-    } else {
+    } else { // idle
       player.sprite.setVelocityX(0);
       player.playAnimation('idle');
     }
   
+    // jump
     if (cursors.up.isDown && player.sprite.body.touching.down) {
       player.sprite.setVelocityY(-250);
     }
 
+    // play animation when jumping or falling
     if (!player.sprite.body.touching.down) 
       player.playAnimation('inTheAir');
 }
